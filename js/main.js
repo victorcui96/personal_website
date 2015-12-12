8a3e9ca9ba502042992e4497b7809304410d26a1
Victor Cui <victor.cui1996@gmail.com>
2015-12-12 15:07:53 -0500

    Update main.js
    Added functionality where user is greeted with a different message (w/ a different color) depending on the time of day.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,42 @@
 
+$(document).ready(function() {
+
+	var time = document.getElementById("time");
+	var today = new Date();
+	var currHour = today.getHours();
+	var currMin = today.getMinutes();
+	var unc = $("#UNC_2018");
+	if (currHour === 6 && currMin >= 30) {
+		time.textContent = "Good morning.";
+		changeColor(time, "blanchedalmond");
+	}
+	else if (currHour >= 7 && (currHour <= 11)) {
+		time.textContent = "Good morning.";
+		changeColor(time, "yellow");
+	}
+	else if (currHour >= 12 && currHour <= 16) {
+		time.textContent = "Good afternoon.";
+		changeColor(time, "darkorange");
+	}
+	else if (currHour >= 17 && currHour <= 20) {
+		time.textContent = "Good evening.";	
+		changeColor(time, "mediumblue");
+	}
+	else if (currHour >= 21 || currHour === 0) {
+		time.textContent = "Good night.";	
+		changeColor(time, "midnightblue");
+	}
+	else {
+		time.textContent = "Hello night owl.";	
+		changeColor(time, "mistyrose");
+		
+	}
+	unc.css("margin-top", "-15px");
+
+	function changeColor(el, newColor) {
+		el.style.color = newColor;
+	}
+	
+
+});
+
